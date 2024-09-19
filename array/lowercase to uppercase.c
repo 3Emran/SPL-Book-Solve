550bffa7e8b10b3a13a08f943c51258dd15c9e7b
emran-hossain <emran.green2037@gmail.com>
2024-09-19 19:52:33 +0600

    uppercase to lowercase convert using array

--- array/lowercase to uppercase.c
+++ array/lowercase to uppercase.c
@@ -1,1 +1,18 @@
+/* A program that reads in a one dimensional character array
+,convert all of item to uppercase, and displayed converted array*/
+#include <stdio.h>
+#include <ctype.h>
+#define SIZE 81
 
+int main()
+{
+    char letter[SIZE];
+    int count;
+    for(count=0; count<SIZE; count++){
+       letter[count] = getchar();
+    }
+    for(count=0; count<SIZE; count++){
+        putchar(toupper(letter[count]));
+    }
+
+}
